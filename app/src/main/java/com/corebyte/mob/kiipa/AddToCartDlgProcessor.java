699c5910a966923eabd9569fed00401c594793c4
AbdulHakeem AbdulAleem <hakeemtunde@hotmail.com>
2019-05-14 22:25:34 +0100

    implement add to cart ui

---
+++ app/src/main/java/com/corebyte/mob/kiipa/AddToCartDlgProcessor.java
@@ -0,0 +1,69 @@
+package com.corebyte.mob.kiipa;
+
+import android.content.Context;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+import com.corebyte.mob.kiipa.model.Measurement;
+import com.corebyte.mob.kiipa.model.Stock;
+
+import java.util.List;
+
+public class AddToCartDlgProcessor {
+
+    private Context mContext;
+    private TextView mStockNameTv;
+    private TextView mPriceTv;
+    private TextView mAvailableQtyTv;
+    private EditText mInputQty;
+    private RadioGroup mMeasurementRg;
+    private Stock mStock;
+    private Measurement mSelectedMeasurement;
+
+    public AddToCartDlgProcessor(Context context, View view, Stock stock) {
+        mContext = context;
+        mMeasurementRg = view.findViewById(R.id.stock_measurement_rg);
+        mStockNameTv = view.findViewById(R.id.stock_name_tv);
+        mPriceTv = view.findViewById(R.id.stock_price_tv);
+        mAvailableQtyTv = view.findViewById(R.id.stock_qty_tv);
+        mInputQty = view.findViewById(R.id.quantity_ed);
+        mStock = stock;
+
+        init();
+    }
+
+    public void attachMeasurements(List<Measurement> measurements) {
+
+        for (final Measurement measurement : measurements ) {
+            RadioButton radioButton = new RadioButton(mContext);
+            radioButton.setText(measurement.getName());
+            radioButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    mPriceTv.setText(String.valueOf(measurement.getSupplyPrice()));
+                    mAvailableQtyTv.setText(String.valueOf(measurement.getSupplyQty()));
+
+                    if (measurement.getSupplyQty() == 0) {
+                        mInputQty.setEnabled(false);
+                    } else {
+                        if (!mInputQty.isEnabled())
+                            mInputQty.setEnabled(true);
+                    }
+
+
+
+                    mSelectedMeasurement = measurement;
+                }
+            });
+            mMeasurementRg.addView(radioButton);
+        }
+
+    }
+
+    private void init() {
+        mStockNameTv.setText(mStock.getName());
+    }
+}
---
+++ app/src/main/java/com/corebyte/mob/kiipa/ui/AddToCartDialogActivity.java
@@ -0,0 +1,60 @@
+package com.corebyte.mob.kiipa.ui;
+
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatDialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Toast;
+
+import com.corebyte.mob.kiipa.AddToCartDlgProcessor;
+import com.corebyte.mob.kiipa.R;
+import com.corebyte.mob.kiipa.model.Measurement;
+import com.corebyte.mob.kiipa.model.Stock;
+import com.corebyte.mob.kiipa.repo.MeasurementCrudOperation;
+import com.corebyte.mob.kiipa.repo.StockCrudOperation;
+
+import java.util.List;
+
+public class AddToCartDialogActivity extends DialogFragment implements AlertDialog.OnClickListener{
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+
+        Stock stock = null;
+        Bundle bundle = getArguments();
+        if (bundle != null && bundle.containsKey(StockItemActivity.STOCKITEM)) {
+            stock = bundle.getParcelable(StockItemActivity.STOCKITEM);
+        }
+
+        AlertDialog.Builder dialog = new AlertDialog.Builder(getActivity());
+        dialog.setTitle("Add To Cart");
+
+        LayoutInflater inflater = requireActivity().getLayoutInflater();
+        View view = inflater.inflate(R.layout.dialog_addtocart, null);
+        dialog.setView(view);
+
+        MeasurementCrudOperation measurementCrudOperation = new MeasurementCrudOperation(getContext());
+        List<Measurement> measurements = measurementCrudOperation.findByStockId(stock.id);
+
+        AddToCartDlgProcessor processor = new AddToCartDlgProcessor(getContext(), view, stock);
+        processor.attachMeasurements(measurements);
+
+        dialog.setPositiveButton(getString(R.string.ok_string), this);
+        dialog.setNegativeButton(getString(R.string.cancel_string), null);
+
+        return dialog.create();
+    }
+
+    @Override
+    public void onClick(DialogInterface dialogInterface, int i) {
+        Toast.makeText(getContext(), "Add to Cart!", Toast.LENGTH_SHORT).show();
+    }
+}
