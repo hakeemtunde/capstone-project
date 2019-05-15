1d19ce9429bbb65191eba42505ea70d305dce62c
AbdulHakeem AbdulAleem <hakeemtunde@hotmail.com>
2019-05-16 00:45:09 +0100

    implement CartStock as parcelable object

--- app/src/main/java/com/corebyte/mob/kiipa/model/CartStock.java
+++ app/src/main/java/com/corebyte/mob/kiipa/model/CartStock.java
@@ -1,17 +1,43 @@
 package com.corebyte.mob.kiipa.model;
 
-public class CartStock {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class CartStock implements Parcelable {
+
+    static Parcelable.Creator<CartStock> CREATOR = new Parcelable.Creator<CartStock>(){
+
+        @Override
+        public CartStock createFromParcel(Parcel parcel) {
+            return new CartStock(parcel);
+        }
+
+        @Override
+        public CartStock[] newArray(int size) {
+            return new CartStock[size];
+        }
+    };
 
     private Long mStockId;
     private Long mId;
     private int mQuantity;
     private double mCostPerStock;
     private double mTotalCost;
+
     private String mStockName;
     private String mMeasureName;
 
-
     private CartStock() {
+    }
+
+    public CartStock(Parcel parcel) {
+        mStockId = parcel.readLong();
+        mId = parcel.readLong();
+        mQuantity = parcel.readInt();
+        mCostPerStock = parcel.readDouble();
+        mTotalCost = parcel.readDouble();
+        mStockName = parcel.readString();
+        mMeasureName = parcel.readString();
     }
 
     public CartStock(Long sId, Long mId, String sName, String mName, double costperstock) {
@@ -38,5 +64,22 @@ public class CartStock {
                 ", mCostPerStock=" + mCostPerStock +
                 ", mTotalCost=" + mTotalCost +
                 '}';
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+
+        parcel.writeLong(mStockId);
+        parcel.writeLong(mId);
+        parcel.writeInt(mQuantity);
+        parcel.writeDouble(mCostPerStock);
+        parcel.writeDouble(mTotalCost);
+        parcel.writeString(mStockName);
+        parcel.writeString(mMeasureName);
     }
 }
