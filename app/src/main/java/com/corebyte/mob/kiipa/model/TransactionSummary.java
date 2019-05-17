303290b22aad6ec1c1ad2ae2d0c9d494fbc08095
AbdulHakeem AbdulAleem <hakeemtunde@hotmail.com>
2019-05-17 15:04:00 +0100

    implement TransactionSummary model

---
+++ app/src/main/java/com/corebyte/mob/kiipa/model/TransactionSummary.java
@@ -0,0 +1,25 @@
+package com.corebyte.mob.kiipa.model;
+
+import android.arch.persistence.room.Entity;
+
+@Entity(tableName = "transaction_summary")
+public class TransactionSummary extends BaseModel {
+
+    double total;
+    int salesOrder;
+
+    public TransactionSummary(double ttl, int sorder) {
+        total = ttl;
+        salesOrder = sorder;
+
+        initDates();
+    }
+
+    public double getTotal() {
+        return total;
+    }
+
+    public int getSalesOrder() {
+        return salesOrder;
+    }
+}
