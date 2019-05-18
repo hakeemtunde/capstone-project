ae33c6a63e16a554b62831be0411cdb157a49b1a
AbdulHakeem AbdulAleem <hakeemtunde@hotmail.com>
2019-05-18 12:54:56 +0100

    create transaction ui

---
+++ app/src/main/java/com/corebyte/mob/kiipa/ui/TransactionSummaryActivity.java
@@ -0,0 +1,15 @@
+package com.corebyte.mob.kiipa.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.corebyte.mob.kiipa.R;
+
+public class TransactionSummaryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_transaction_summary);
+    }
+}
