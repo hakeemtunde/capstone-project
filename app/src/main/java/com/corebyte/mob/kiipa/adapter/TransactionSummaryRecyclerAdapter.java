10ead72cc5e985a725683427bedd0c8f913f757b
AbdulHakeem AbdulAleem <hakeemtunde@hotmail.com>
2019-05-18 21:50:30 +0100

    implement recycler adapter for transaction summary and ui

---
+++ app/src/main/java/com/corebyte/mob/kiipa/adapter/TransactionSummaryRecyclerAdapter.java
@@ -0,0 +1,71 @@
+package com.corebyte.mob.kiipa.adapter;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.corebyte.mob.kiipa.R;
+import com.corebyte.mob.kiipa.model.TransactionSummary;
+import com.corebyte.mob.kiipa.util.DateUtil;
+
+import java.util.List;
+
+public class TransactionSummaryRecyclerAdapter extends RecyclerView.Adapter<TransactionSummaryRecyclerAdapter.ViewHolder> {
+
+    private List<TransactionSummary> mTransactionSummaries;
+
+    public TransactionSummaryRecyclerAdapter(List<TransactionSummary> transactionSummaries) {
+        mTransactionSummaries = transactionSummaries;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(viewGroup.getContext())
+                .inflate(R.layout.layout_transaction_summary_list, viewGroup, false);
+
+        ViewHolder viewHolder = new ViewHolder(view);
+
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
+
+        TransactionSummary transactionSummary = mTransactionSummaries.get(i);
+        viewHolder.bind(transactionSummary);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mTransactionSummaries.size();
+    }
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+
+        TextView orderTv;
+        TextView dateTv;
+        TextView amountTv;
+//        ImageView transactionBreakdownIv;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            orderTv = itemView.findViewById(R.id.order_tv);
+            dateTv = itemView.findViewById(R.id.date_tv);
+            amountTv = itemView.findViewById(R.id.amount_tv);
+//            transactionBreakdownIv = itemView.findViewById(R.id.transaction_breakdown_iv);
+        }
+
+        public void bind(TransactionSummary transactionSummary) {
+            orderTv.setText(transactionSummary.getSalesOrder());
+            dateTv.setText(DateUtil.getDateFormat(transactionSummary.createdAt));
+            amountTv.setText(String.valueOf(transactionSummary.getTotal()));
+        }
+    }
+}
--- app/src/main/java/com/corebyte/mob/kiipa/ui/TransactionSummaryActivity.java
+++ app/src/main/java/com/corebyte/mob/kiipa/ui/TransactionSummaryActivity.java
@@ -2,14 +2,29 @@ package com.corebyte.mob.kiipa.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 import com.corebyte.mob.kiipa.R;
+import com.corebyte.mob.kiipa.adapter.TransactionSummaryRecyclerAdapter;
+import com.corebyte.mob.kiipa.repo.TransactionSummaryCrudOp;
 
 public class TransactionSummaryActivity extends AppCompatActivity {
+
+    private TransactionSummaryCrudOp mTransactionSummaryCrudOp;
+    private RecyclerView mTransactionSummaryRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_transaction_summary);
+
+        mTransactionSummaryCrudOp = new TransactionSummaryCrudOp(getApplicationContext());
+        TransactionSummaryRecyclerAdapter adapter = new TransactionSummaryRecyclerAdapter(
+                mTransactionSummaryCrudOp.getAll());
+
+        mTransactionSummaryRecyclerView = findViewById(R.id.transaction_summary_rv);
+        mTransactionSummaryRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mTransactionSummaryRecyclerView.setAdapter(adapter);
     }
 }
